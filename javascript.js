284cf542e1d00d7948b05be0ca961bd9daa60bed
ChriDiet <ch.dietrichs@yahoo.no>
2024-11-26 03:18:40 +0100

    Add quotes around answer. Make all answers lowercase

--- javascript.js
+++ javascript.js
@@ -7,27 +7,30 @@ Print you won/you lost*/
 let answer = prompt("Choose rock, paper or scissors", (" "))
 let ran = Math.floor(Math.random()*3);
 
-if (answer.toLowerCase() === "rock" || answer.toLowerCase() === "paper" || answer.toLowerCase() === "scissors") {
-      console.log("You chose " + (answer.toLowerCase()));
+function getUserChoice () {
+   if (answer?.toLowerCase() === "rock" || answer?.toLowerCase() === "paper" || answer?.toLowerCase() === "scissors") {
+      console.log("You chose " + `\"${(answer.toLowerCase())}\"`);
    } else {
-      console.log("You didn't choose rock, paper or scissors. Try again");
+      alert("You didn't choose rock, paper or scissors. Try again");
    }
+}
 
-
-
+getUserChoice();
 
 function getComputerChoice () {
-   console.log("The computer chose " +(ran))
-} 
+   let compchoice = Math.floor(Math.random() *3 );
+if (compchoice === 0) {
+   console.log('The computer chose "paper"');
+} else if (compchoice === 1) {
+   console.log('The computer chose "rock"');
+} else if (compchoice === 2) {
+   console.log('The computer chose "scissors"');
+}
 
-   if (ran === 1) {
-      console.log("Paper");
-   } else if (ran === 2) {
-      console.log("Rock");
-   } else if (ran === 0) {
-      console.log("Scissors");
-   }
+}
 
+getComputerChoice();
 
+console.log("And the winner is......");
 
 

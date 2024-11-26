8e7d7a96826b9f2750ef626f5135e330984e750b
ChriDiet <ch.dietrichs@yahoo.no>
2024-11-26 02:33:39 +0100

    Add if/else to only accept rock, paper or scissor
    User input could be whatever, now it can only be rock paper or scissor.

--- javascript.js
+++ javascript.js
@@ -4,24 +4,30 @@ Computer generate random choice with function getComputerChocie
 Print computers choice and users choice
 Determine which one is the winner 
 Print you won/you lost*/
-let answer = prompt("Choose rock, paper or scissors")
-let newAnswer = answer.toLowerCase();
-let finalAnswer = `"${newAnswer}"`;
-
-function getUserChoice() {
-   console.log("You chose " + (finalAnswer))
-}
-getUserChoice();
-function getComputerChoice () {
-   console.log("The computer chose" +(ran))
-}
+let answer = prompt("Choose rock, paper or scissors", (" "))
 let ran = Math.floor(Math.random()*3);
 
-if (ran === 1) {
-   console.log("Paper");
-} else if (ran === 2) {
-   console.log("Rock");
-} else if (ran === 0) {
-   console.log("Scissors");
-}
+if (answer.toLowerCase() === "rock" || answer.toLowerCase() === "paper" || answer.toLowerCase() === "scissors") {
+      console.log("You chose " + (answer.toLowerCase()));
+   } else {
+      console.log("You didn't choose rock, paper or scissors. Try again");
+   }
+
+
+
+
+function getComputerChoice () {
+   console.log("The computer chose " +(ran))
+} 
+
+   if (ran === 1) {
+      console.log("Paper");
+   } else if (ran === 2) {
+      console.log("Rock");
+   } else if (ran === 0) {
+      console.log("Scissors");
+   }
+
+
+
 

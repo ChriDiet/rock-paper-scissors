7e75a338a026e42e395f2a2b8ada9ff958bc75fd
ChriDiet <ch.dietrichs@yahoo.no>
2024-11-26 17:09:05 +0100

    Add function to getHumanChoice and playRound function
    Rewrote getHumanChoice function to a switch
    statement and also made playRound function to
    output "you won" "you lost" or "tie".

--- javascript.js
+++ javascript.js
@@ -8,16 +8,46 @@ Print you won/you lost*/
 function getComputerChocie() {
    let random = (Math.floor(Math.random () * 3));
    switch (random) {
-   case 0:
-      random = "rock";
-      break;
-   case 1:
-      random = "paper";
-      break;
-   case 2:
-      random = "scissors"; }
-return(random);
+      case 0:
+         random = "rock";
+         break;
+      case 1:
+         random = "paper";
+         break;
+      case 2:
+         random = "scissors"; }
+   return(random);
 }
 
+function getHumanChoice () {
+let answer = prompt ("Pick rock, paper or scissors!", " ");
+   switch (answer?.toLowerCase()) {
+      case "rock":
+      case "paper":
+      case "scissors":
+         return(answer?.toLowerCase());
+      break;
+      default:
+         alert("You didn't pick one");
+   }
+}  
 
+let humanScore = 0;
+let computerScore = 0;
 
+function playRound (humanChoice, computerChoice) {
+if (humanChoice === "rock" && computerChoice === "paper" || 
+   humanChoice === "paper" && computerChoice === "scissors" ||
+   humanChoice === "scissors" && computerChoice === "rock") {
+   console.log("You lose");
+} else if (humanChoice === "paper" && computerChoice === "rock" || humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "paper") {
+   console.log("You won!");
+} else { 
+   console.log("Its a tie");
+}
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChocie();
+
+playRound(humanSelection, computerSelection);

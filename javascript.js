ea8789205282e353d7b012ba5703fd873053fded
ChriDiet <ch.dietrichs@yahoo.no>
2024-11-26 16:08:19 +0100

    Delete code, start over
    I deleted my previous code to start over with a
    different take. I think my getComputerChoice is
    more correct this time around, with a switch
    statement inside to get GetComputerChoice to return
    "paper" "scissors" or "rock".

--- javascript.js
+++ javascript.js
@@ -4,33 +4,20 @@ Computer generate random choice with function getComputerChocie
 Print computers choice and users choice
 Determine which one is the winner 
 Print you won/you lost*/
-let answer = prompt("Choose rock, paper or scissors", (" "))
-let ran = Math.floor(Math.random()*3);
 
-function getUserChoice () {
-   if (answer?.toLowerCase() === "rock" || answer?.toLowerCase() === "paper" || answer?.toLowerCase() === "scissors") {
-      console.log("You chose " + `\"${(answer.toLowerCase())}\"`);
-   } else {
-      alert("You didn't choose rock, paper or scissors. Try again");
-   }
+function getComputerChocie() {
+   let random = (Math.floor(Math.random () * 3));
+   switch (random) {
+   case 0:
+      random = "rock";
+      break;
+   case 1:
+      random = "paper";
+      break;
+   case 2:
+      random = "scissors"; }
+return(random);
 }
 
-getUserChoice();
-
-function getComputerChoice () {
-   let compchoice = Math.floor(Math.random() *3 );
-if (compchoice === 0) {
-   console.log('The computer chose "paper"');
-} else if (compchoice === 1) {
-   console.log('The computer chose "rock"');
-} else if (compchoice === 2) {
-   console.log('The computer chose "scissors"');
-}
-
-}
-
-getComputerChoice();
-
-console.log("And the winner is......");
 
 

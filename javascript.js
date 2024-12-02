fd39c1ffd49bb78ebfb108a59d4ef7bddb7d4403
ChriDiet <ch.dietrichs@yahoo.no>
2024-12-02 23:36:50 +0100

    Edit code to make it a little cleaner

--- javascript.js
+++ javascript.js
@@ -2,7 +2,6 @@ let humanScore = 0;
 let computerScore = 0;
 let humanChoice;
 let computerChoice;
-let i = 0;
 let humanAnswer;
 
 function getHumanChoice() {
@@ -30,26 +29,48 @@ function getComputerChoice() {
 function playRound (computerPick, humanPick) {
    humanPick = getHumanChoice();
    computerPick = getComputerChoice();
+
    if (humanPick === computerPick) {
-      alert ("You chose " + humanPick + "\n" + "The computer chose " + computerPick + "\n" + "Same choice! It's a tie!" + "\n" + "Your score: " + humanScore + " | " + "Computer score: " + computerScore);
+      alert ("You chose " + humanPick + "\n" + "The computer chose " + computerPick + 
+            "\n" + "Same choice! It's a tie!" + "\n" + "Your score: " + humanScore + 
+            " | " + "Computer score: " + computerScore);
+
    } else if (humanPick === "rock" && computerPick === "paper") {
       ++computerScore;
-      alert("You chose " + humanPick + "\n" + "The computer chose " + computerPick + "\n" + "Paper covers rock! You lose!" + "\n" + "Your score: " + humanScore + " | " + "Computer score: " + computerScore);
+      alert ("You chose " + humanPick + "\n" + "The computer chose " + computerPick + 
+            "\n" + "Paper covers rock! You lose!" + "\n" + "Your score: " + humanScore + 
+            " | " + "Computer score: " + computerScore);
+
    } else if (humanPick === "paper" && computerPick === "scissors") {
       ++computerScore;
-      alert("You chose " + humanPick + "\n" + "The computer chose " + computerPick + "\n" + "Scissors cuts paper! You lose!" + "\n" + "Your score: " + humanScore + " | " + "Computer score: " + computerScore);
+      alert ("You chose " + humanPick + "\n" + "The computer chose " + computerPick + 
+            "\n" + "Scissors cuts paper! You lose!" + "\n" + "Your score: " + humanScore + 
+            " | " + "Computer score: " + computerScore);
+
    } else if (humanPick === "scissors" && computerPick === "rock") {
       ++computerScore;
-      alert("You chose " + humanPick + "\n" + "The computer chose " + computerPick + "\n" + "Rock smashes scissors! You lose!" + "\n" + "Your score: " + humanScore + " | " + "Computer score: " + computerScore);
+      alert ("You chose " + humanPick + "\n" + "The computer chose " + computerPick + 
+            "\n" + "Rock smashes scissors! You lose!" + "\n" + "Your score: " + humanScore + 
+            " | " + "Computer score: " + computerScore);
+
    } else if (computerPick === "rock" && humanPick === "paper") {
       ++humanScore;
-      alert("You chose " + humanPick + "\n" + "The computer chose " + computerPick + "\n" + "Paper covers rock! You win!" + "\n" + "Your score: " + humanScore + " | " + "Computer score: " + computerScore);
+      alert ("You chose " + humanPick + "\n" + "The computer chose " + computerPick + 
+            "\n" + "Paper covers rock! You win!" + "\n" + "Your score: " + humanScore + 
+            " | " + "Computer score: " + computerScore);
+
    } else if (computerPick === "paper" && humanPick === "scissors") {
       ++humanScore;
-      alert("You chose " + humanPick + "\n" + "The computer chose " + computerPick + "\n" + "Scissors cuts paper! You win!" + "\n" + "Your score: " + humanScore + " | " + "Computer score: " + computerScore);
+      alert ("You chose " + humanPick + "\n" + "The computer chose " + computerPick + 
+            "\n" + "Scissors cuts paper! You win!" + "\n" + "Your score: " + humanScore + 
+            " | " + "Computer score: " + computerScore);
+
    } else if (computerPick === "scissors" && humanPick === "rock") {
       ++humanScore;
-      alert("You chose " + humanPick + "\n" + "The computer chose " + computerPick + "\n" + "Rock smashes scissors! You win!" + "\n" + "Your score: " + humanScore + " | " + "Computer score: " + computerScore);
+      alert ("You chose " + humanPick + "\n" + "The computer chose " + computerPick + 
+            "\n" + "Rock smashes scissors! You win!" + "\n" + "Your score: " + humanScore + 
+            " | " + "Computer score: " + computerScore);
+
    } else if (humanPick !== "rock" || humanPick !== "paper" || humanPick !== "scissors") {
       alert("Invalid choice")
    }
@@ -62,12 +83,18 @@ console.log(playRound());
 console.log(playRound());
 console.log(playRound());
 console.log(playRound());
+
 if (humanScore < computerScore) {
-   alert("Game over! You lost!" + "\n" + "Finale score is: " + "\n" + "You: " + humanScore + " | " + "Computer: " + computerScore);
+   alert ("Game over! You lost!" + "\n" + "Finale score is: " + "\n" + "You: " + 
+         humanScore + " | " + "Computer: " + computerScore);
+
 } else if (humanScore > computerScore) {
-   alert("Game over! You won!" + "\n" + "Finale score is: " + "\n" + "You: " + humanScore + " | " + "Computer: " + computerScore);
+   alert ("Game over! You won!" + "\n" + "Finale score is: " + "\n" + "You: " + 
+         humanScore + " | " + "Computer: " + computerScore);
+
 } else if (humanScore == computerScore) {
-   alert("Game over! It's a tie!" + "\n" + "Finale score is: " + "\n" + "You: " + humanScore + " | " + "Computer: " + computerScore);
+   alert ("Game over! It's a tie!" + "\n" + "Finale score is: " + "\n" + "You: " + 
+         humanScore + " | " + "Computer: " + computerScore);
 }
 }
 
